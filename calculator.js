f2e831fe9e06143c00401e0729be5aa36eff79cb
joannjiang <qiongrongjiang@gmail.com>
2017-03-20 19:46:05 -0400

    Add validation of args

--- calculator.js
+++ calculator.js
@@ -7,8 +7,35 @@ const categoryMap = {
   other: 0
 }
 
+const error = function (msg) {
+  return {
+    done: false,
+    msg: msg
+  }
+}
+
+const isUndefined = function (val) {
+  return typeof val === 'undefined'
+}
+
 module.exports = {
   calcCost: function (basePrice, personNum, category = 'other') {
+    // validate args
+    if (isUndefined(basePrice) || isUndefined(personNum )) {
+      return error('arg missing')
+    }
+
+    if (typeof basePrice !== 'number' || basePrice <= 0) {
+      return error('invalid price')
+    }
+
+    if (typeof personNum !== 'number' || personNum <= 0 || !Number.isInteger(personNum)) {
+      return error('invalid personNum')
+    }
+
+    if (['food', 'drugs', 'electronics', 'other'].indexOf(category) === -1) {
+      return error('invalid category')
+    }
 
     // Main calculation
     const base = basePrice + basePrice * 0.05
@@ -21,6 +48,9 @@ module.exports = {
     // round to two decimal places
     output = output.toFixed(2)
 
-    return output * 1
+    return {
+      done: true,
+      result: output * 1
+    }
   }
 }
